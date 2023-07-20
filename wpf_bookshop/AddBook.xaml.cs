ee9ee5e13e5a6cea7243abfccd62f78996fad034
kutukov2000 <kutukov16.2005@gmail.com>
2023-07-20 11:40:51 +0300

    Clean Code

--- wpf_bookshop/AddBook.xaml.cs
+++ wpf_bookshop/AddBook.xaml.cs
@@ -1,23 +1,8 @@
 ﻿using exam_entity.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace wpf_bookshop
 {
-    /// <summary>
-    /// Interaction logic for AddBook.xaml
-    /// </summary>
     public partial class AddBook : Window
     {
         public Book Book { get; set; }
@@ -34,7 +19,7 @@ namespace wpf_bookshop
             Book.AuthorId = int.Parse(author.Text);
             Book.GenreId = int.Parse(genre.Text);
             Book.Title = title.Text;
-            Book.PublicationYear=int.Parse(year.Text);
+            Book.PublicationYear = int.Parse(year.Text);
 
             DialogResult = true;
             this.Close();
