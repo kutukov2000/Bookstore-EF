8419042bb8aecf7331e3ee3e9e6dd930d2caf6cf
kutukov2000 <kutukov16.2005@gmail.com>
2023-07-20 11:27:02 +0300

    Update ViewModel

--- wpf_bookshop/MainWindow.xaml.cs
+++ wpf_bookshop/MainWindow.xaml.cs
@@ -1,120 +1,12 @@
-﻿using exam_entity;
-using exam_entity.Entities;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace wpf_bookshop
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        ViewModel model = new ViewModel();
-
         public MainWindow()
         {
             InitializeComponent();
-
-            this.DataContext = model;
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            datagrid.ItemsSource = model.Authors;
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            datagrid.ItemsSource = model.Books;
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            datagrid.ItemsSource = model.Countries;
-        }
-
-        private void Button_Click_3(object sender, RoutedEventArgs e)
-        {
-            datagrid.ItemsSource = model.Genres;
-        }
-
-        private void Button_Click_4(object sender, RoutedEventArgs e)
-        {
-            datagrid.ItemsSource = model.BooksInStore;
-        }
-
-        private void Button_Click_6(object sender, RoutedEventArgs e)
-        {
-            if(checkIsReadOnly.IsChecked != true)
-            {
-            model.DeleteSelectedBooks(datagrid.SelectedItems.Cast<Book>().ToList());
-            
-            }
-            datagrid.ItemsSource = model.Books;
-        }
-
-        private void datagrid_LostFocus(object sender, RoutedEventArgs e)
-        {
-            model.SaveChanges();
-        }
-
-        private void Button_Click_5(object sender, RoutedEventArgs e)
-        {
-            AddBook addBook = new AddBook();
-            if (addBook.ShowDialog() == true)
-            {
-                model.AddBook(addBook.Book);
-            }
-            datagrid.ItemsSource = model.Books;
-        }
-    }
-    class ViewModel
-    {
-        private BookShopDbContext context = new BookShopDbContext();
-
-        public ICollection<Book> Books => context.Books.ToList();
-        public ICollection<Author> Authors => context.Authors.ToList();
-        public ICollection<Genre> Genres => context.Genres.ToList();
-        public ICollection<Country> Countries => context.Countries.ToList();
-        public ICollection<BookInStore> BooksInStore => context.BooksInStore.ToList();
-        public ViewModel() { }
-        public void DeleteSelectedBooks(ICollection<Book> booksToDelete)
-        {
-            foreach (Book book in booksToDelete)
-            {
-                context.Books.Remove(book);
-            }
-
-            SaveChanges();
-        }
-        public void SaveChanges()
-        {
-            try
-            {
-                context.SaveChanges();
-            }
-            catch (Exception) { }
-        }
-        public void AddBook(Book newBook)
-        {
-            context.Books.Add(newBook);
-            SaveChanges();
         }
     }
 }
---
+++ wpf_bookshop/ViewModel.cs
@@ -0,0 +1,98 @@
+﻿using exam_entity;
+using exam_entity.Entities;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace wpf_bookshop
+{
+    [AddINotifyPropertyChangedInterface]
+    class ViewModel
+    {
+        private BookShopDbContext context = new BookShopDbContext();
+
+        public object CurrentTable { get; set; }
+        public Book SelectedBook { get; set; }
+
+        public ICollection<Book> booksToDelete { get; set; }
+        public ICollection<Book> Books => context.Books.ToList();
+        public ICollection<Author> Authors => context.Authors.ToList();
+        public ICollection<Genre> Genres => context.Genres.ToList();
+        public ICollection<Country> Countries => context.Countries.ToList();
+        public ICollection<BookInStore> BooksInStore => context.BooksInStore.ToList();
+
+        public RelayCommand SaveChangesCommand { get; set; }
+        public RelayCommand BooksTableCommand { get; set; }
+        public RelayCommand AuthorsTableCommand { get; set; }
+        public RelayCommand GenresTableCommand { get; set; }
+        public RelayCommand CountriesTableCommand { get; set; }
+        public RelayCommand BooksInStoreTableCommand { get; set; }
+        public RelayCommand AddBookCommand { get; set; }
+        public RelayCommand DeleteBookCommand { get; set; }
+        public ViewModel()
+        {
+            CurrentTable = Books;
+
+            SaveChangesCommand = new RelayCommand(o =>
+            {
+                SaveChanges();
+            });
+
+            BooksTableCommand = new RelayCommand(o =>
+            {
+                CurrentTable = Books;
+            });
+
+            AuthorsTableCommand = new RelayCommand(o =>
+            {
+                CurrentTable = Authors;
+            });
+
+            GenresTableCommand = new RelayCommand(o =>
+            {
+                CurrentTable = Genres;
+            });
+
+            CountriesTableCommand = new RelayCommand(o =>
+            {
+                CurrentTable = Countries;
+            });
+
+            BooksInStoreTableCommand = new RelayCommand(o =>
+            {
+                CurrentTable = BooksInStore;
+            });
+
+            AddBookCommand = new RelayCommand(o =>
+            {
+                AddBook addBook = new AddBook();
+
+                if (addBook.ShowDialog() == true)
+                {
+                    context.Books.Add(addBook.Book);
+                    SaveChanges();
+                }
+                CurrentTable = Books;
+            });
+
+            DeleteBookCommand = new RelayCommand(o =>
+            {
+                if (SelectedBook is not null)
+                {
+                    context.Books.Remove(SelectedBook);
+                    SaveChanges();
+                }
+                CurrentTable = Books;
+            });
+        }
+        public void SaveChanges()
+        {
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (Exception) { }
+        }
+    }
+}
